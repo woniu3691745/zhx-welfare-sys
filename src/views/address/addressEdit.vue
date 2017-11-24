f1c833ff4cca22f718bd094857f23c262f6e7ac6
xuqingyou <346823358@qq.com>
2017-11-24 09:27:05 +0800

    地址管理

--- src/views/address/addressEdit.vue
+++ src/views/address/addressEdit.vue
@@ -10,16 +10,21 @@
     <div class="common-header">
       <mt-header title="编辑收货人">
         <router-link to="/mine" slot="left" fixed>
-          <mt-button icon="back">返回</mt-button>
+          <mt-button icon="back"></mt-button>
         </router-link>
       </mt-header>
     </div>
-    <div class="body">
-
+    <div class="addAddress-body">
+      <mt-field class="border-1px" label="收货人" placeholder="收货人姓名" v-model="consignee"></mt-field>
+      <mt-field class="border-1px" label="联系电话" placeholder="请输入手机号" v-model="phoneNum"></mt-field>
+      <mt-cell class="border-1px height-88" title="所在地区" is-link to="" value="">
+      </mt-cell>
+      <mt-field placeholder="请填写详细地址，不少于5个字" type="textarea" rows="4" v-model="detailedAddress"></mt-field>
     </div>
     <div class="bottom">
-
+        保存
     </div>
+    
   </div>
 </template>
 
@@ -33,7 +38,12 @@
    props: {},
    // 变量
    data () {
-     return {}
+     return {
+       consignee: '',
+       phoneNum: '',
+       detailedAddress: '',
+       value: ''
+     }
    },
    computed: {},
    // 使用其它组件
@@ -48,5 +58,33 @@
 </script>
 
 <style lang="less" scoped>
-
+.addAddress-body {
+  .hheight-22 {
+    height: 0.22rem;
+    background: #F5F5F5;
+  } 
+  .mint-cell{
+    padding: 0 0.28rem;
+    
+    .mint-cell-wrapper {
+      .mint-cell-title {
+        font-size: 0.28rem;
+      }
+    }
+    
+  }
+  
+}
+.bottom {
+  background: #FD404A;
+  width: 100%;
+  height: 0.88rem;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  color: #ffffff;
+  font-size: 0.32rem;
+  text-align: center;
+  line-height: 0.88rem;
+}
 </style>
--- src/views/address/addressManage.vue
+++ src/views/address/addressManage.vue
@@ -14,19 +14,32 @@
           </router-link>
         </mt-header>
       </div>
-    <div class="body">
-      <div style="font-size:14px;">
-        <span>李娜</span>
-        <mt-radio
-          v-model="value"
-          :options="['设为默认地址']">
-        </mt-radio>
-        <span @click="edit">编辑</span>
-        <span @click="del">删除</span>
+    <div class="address-manage-body">
+      <div>
+        <div class="name-tel clear">
+          <span class="left">李娜</span>
+          <span class="right">12345678900</span>
+        </div>
+        <p class="address-words">北京市西城区金融街20号交通大厦B座10层资和信电子支付有限公司</p>
+        <div class="delete-container border-top-1px">
+          <mt-radio
+            v-model="value"
+            :options="['设为默认地址']">
+          </mt-radio>
+          <div class="clear editor-delete-container">
+            <span class="editor left" @click="edit">编辑</span>
+            <span class="address-delete-editor right" @click="del">删除</span>
+          </div>
+        </div>
+        <div class="height-20">
+
+        </div>
       </div>
     </div>
     <div class="bottom">
-
+      <div>
+        添加收货地址
+      </div>
     </div>
   </div>
 </template>
@@ -65,5 +78,52 @@
 </script>
 
 <style lang="less" scoped>
-
+.addressManage {
+  .common-header {
+    width: 100%;
+    position: fixed;
+    left: 0;
+    top: 0;
+  }
+  .address-manage-body {
+    padding-top: 0.88rem;
+    margin-bottom: 1.08rem;
+    .name-tel {
+      padding: 0 0.2rem;
+      span {
+        height: 0.84rem;
+        line-height: 0.84rem;
+        font-size: 0.28rem;
+        color: #323232;
+      }
+    }
+    .address-words {
+      font-size: 0.24rem;
+      color: #323232;
+      line-height: 0.4rem;
+      padding: 0 0.2rem 0.2rem;
+      box-shadow: inset 0 -1px 0 0 rgba(220,220,220,0.50);
+      
+    }
+    .height-20 {
+      height: 0.2rem;
+      background: #f5f5f5;
+    }
+  }
+  .bottom {
+    position: fixed;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    div {
+      height: 0.88rem;    
+      background: #FD404A;
+      font-size: 0.32rem;
+      color: #FFFFFF;
+      text-align: center;
+      line-height: 0.88rem;
+    }
+    
+  }
+}
 </style>

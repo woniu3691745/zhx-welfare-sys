7be0969117df22f57a8f984be23de50893216352
lidl <3691745@qq.com>
2017-12-08 18:41:09 +0800

    轮播图

--- src/views/main/mall.vue
+++ src/views/main/mall.vue
@@ -2,7 +2,7 @@
  * @Author: lidongliang 
  * @Date: 2017-10-12 17:58:36 
  * @Last Modified by: lidongliang
- * @Last Modified time: 2017-12-08 18:32:41
+ * @Last Modified time: 2017-12-08 18:40:35
  * 首页组件
  */
 <template>
@@ -24,16 +24,28 @@
     <ul class="clear body-containers" v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="50">
       <li class="solid-top">
         <div class="index-swipe">
-          <mt-swipe :auto="2000">
-            <mt-swipe-item><img src="../../assets/swipe/1.jpg"></img>
+          <mt-swipe :auto="2000" v-if="this.typeIdAll === '1000002'">
+            <mt-swipe-item><img src="../../assets/tu10.png"></img>
             </mt-swipe-item>
-            <mt-swipe-item><img src="../../assets/swipe/2.jpg"></img>
+            <mt-swipe-item><img src="../../assets/tu11.png"></img>
             </mt-swipe-item>
-            <mt-swipe-item><img src="../../assets/swipe/3.jpg"></img>
+            <mt-swipe-item><img src="../../assets/tu12.png"></img>
             </mt-swipe-item>
-            <mt-swipe-item><img src="../../assets/swipe/4.jpg"></img>
+          </mt-swipe>
+           <mt-swipe :auto="2000" v-else-if="this.typeIdAll === '1000001'">
+            <mt-swipe-item><img src="../../assets/tu4.png"></img>
             </mt-swipe-item>
-            <mt-swipe-item><img src="../../assets/swipe/5.jpg"></img>
+            <mt-swipe-item><img src="../../assets/tu5.png"></img>
+            </mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/tu6.png"></img>
+            </mt-swipe-item>
+          </mt-swipe>
+           <mt-swipe :auto="2000" v-else-if="this.typeIdAll === '1000003'">
+            <mt-swipe-item><img src="../../assets/tu1.png"></img>
+            </mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/tu2.png"></img>
+            </mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/tu3.png"></img>
             </mt-swipe-item>
           </mt-swipe>
         </div>
